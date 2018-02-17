8fb73f7732e4c4543d9cc4c4e5102aa25935d1ba
Leanna <leannasmith710@gmail.com>
2018-02-17 17:51:24 -0500

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured, dates) VALUES ('chicken burger', 'yes', 12.00);
+INSERT INTO burgers (burger_name, devored, dates) VALUES ('bacon burger', 'yes', 9.30);
+INSERT INTO burgers (burger_name, devoured, dates) VALUES ('veggie burger', 'no', 6.15);
